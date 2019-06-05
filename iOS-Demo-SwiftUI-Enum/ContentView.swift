f9542e0274624792450ffc164e8def37ae7f30a3
Rob Lokhorst <roblokhorst@gmail.com>
2019-06-05 14:23:27 +0200

    Initial Commit

---
+++ iOS-Demo-SwiftUI-Enum/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iOS-Demo-SwiftUI-Enum
+//
+//  Created by Rob Lokhorst on 05/06/2019.
+//  Copyright © 2019 Rob Lokhorst. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
