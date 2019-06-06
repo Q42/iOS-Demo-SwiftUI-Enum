9e01d92725dae816be89d0b21933743d53723697
Rob Lokhorst <roblokhorst@gmail.com>
2019-06-06 14:59:40 +0200

    Start with generating enum views

---
+++ iOS-Demo-SwiftUI-Enum/Sourcery/Output/EnumViews.generated.swift
@@ -0,0 +1,15 @@
+// Generated using Sourcery 0.16.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+// MARK - Generate SwiftUI Views for Enums
+
+import SwiftUI
+
+internal extension Row {
+  struct Switch<T0: View, T1: View, T2: View, T3: View>: View {
+    var body: TupleView<(T1, T2, T3)>
+
+    // HELP !1!!
+  }
+}
+
